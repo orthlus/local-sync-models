c592fe1ae50268ace90f1e943a2c4489c574f26c
ort <orthodoxplus@ya.ru>
2025-05-12 22:37:20 +0300

    K8sIngressRoute

--- src/main/java/art/aelaort/models/servers/k8s/K8sIngressRoute.java
+++ src/main/java/art/aelaort/models/servers/k8s/K8sIngressRoute.java
@@ -3,17 +3,20 @@ package art.aelaort.models.servers.k8s;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.jackson.Jacksonized;
 
 import java.util.List;
 
 @Getter
-@Builder
+@Builder(toBuilder = true)
 @Jacksonized
-@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonIgnoreProperties(ignoreUnknown = true, value = "tls", allowSetters = true)
 public class K8sIngressRoute {
 	private String name;
 	private String namespace;
 	private List<Route> routes;
 	private boolean hasTls;
+	@Setter
+	private Object tls;
 }
