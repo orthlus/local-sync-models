eefffd080bdfd4369b0eb21f8e99175b59aaead2
ort <orthodoxplus@ya.ru>
2025-10-23 07:01:22 +0300

    K8sIngressRoute

--- src/main/java/art/aelaort/models/servers/k8s/K8sIngressRoute.java
+++ src/main/java/art/aelaort/models/servers/k8s/K8sIngressRoute.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.extern.jackson.Jacksonized;
 
+import java.util.Objects;
+
 @Getter
 @Builder(toBuilder = true)
 @Jacksonized
@@ -16,4 +18,18 @@ public class K8sIngressRoute {
 	private String serviceName;
 	private Integer servicePort;
 	private boolean hasTls;
+
+	@Override
+	public final boolean equals(Object o) {
+		if (!(o instanceof K8sIngressRoute that)) {
+			return false;
+		}
+
+		return Objects.equals(match, that.match);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(match);
+	}
 }
