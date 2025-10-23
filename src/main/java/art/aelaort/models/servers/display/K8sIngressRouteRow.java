40317808402702158febbd1646d1f7652246ea06
ort <orthodoxplus@ya.ru>
2025-10-23 05:05:02 +0300

    K8sIngressRouteRow

---
+++ src/main/java/art/aelaort/models/servers/display/K8sIngressRouteRow.java
@@ -0,0 +1,19 @@
+package art.aelaort.models.servers.display;
+
+import java.util.Comparator;
+
+public record K8sIngressRouteRow(
+		String cluster,
+		String namespace,
+		String image,
+		String name,
+		String route
+) implements Comparable<K8sIngressRouteRow> {
+	@Override
+	public int compareTo(K8sIngressRouteRow o) {
+		return Comparator.comparing(K8sIngressRouteRow::cluster)
+				.thenComparing(K8sIngressRouteRow::namespace)
+				.thenComparing(K8sIngressRouteRow::name)
+				.compare(this, o);
+	}
+}
